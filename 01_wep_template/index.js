beda04482da6d5319bb2f3692a71a4fac1392947
simmoe <simmoe@gmail.com>
2025-10-06 12:53:07 +0200

    menu click

---
+++ 01_wep_template/index.js
@@ -0,0 +1,19 @@
+var currentPage = '#page5'
+
+//P5 setup() bliver kaldt EN gang før siden vises 
+function setup(){
+    console.log('P5 setup kaldt')
+    //Sæt event listeners op på menu
+    select('#menu-side1').mousePressed(
+        function(){
+            shiftPage('#page1')
+        }
+    )
+}
+
+
+function shiftPage(newPage){
+    select(currentPage).removeClass('show')
+    select(newPage).addClass('show')
+    currentPage = newPage
+}
