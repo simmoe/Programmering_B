7df3eaf1b04163225bda3f7978cfed7f75fe01e8
simmoe <simmoe@gmail.com>
2025-12-18 09:45:56 +0100

    begun game template

---
+++ template_game/script.js
@@ -0,0 +1,23 @@
+//We select the gamecontainer from html - and save it in a var called game_container
+var game_container = document.querySelector('#game-container')
+
+//The function takes a asta div element as argument, and removeds it from its parent container  
+function killAsta(asta){
+    game_container.removeChild(asta)
+}
+
+//setInterval is a javascript function that runs a function every X milliseconds 
+//in this case we use the function to make new image elements and put them inside the game_container
+setInterval(()=>{
+    //vi laver et img element i variablen new_asta
+    var new_asta = document.createElement('img')
+    //we add a source to the new img 
+    new_asta.src = './assets/asta.png'
+    //we add a classname to it so we can style it 
+    new_asta.className = 'asta'
+    //We put the new img element inside the game containern
+    game_container.appendChild(new_asta)
+    //when we click the new img element, we call the killAsta function which removes it
+    new_asta.addEventListener('click', ()=>killAsta(new_asta))
+}, 1250)
+
