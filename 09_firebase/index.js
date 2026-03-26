4e5f190b647bc93da2ab4e50461bc9621f952501
simmoe <simmoe@gmail.com>
2026-03-26 09:40:25 +0100

    firease projekt

---
+++ 09_firebase/index.js
@@ -0,0 +1,16 @@
+//lav en ref til din collection
+var testRef = db.collection('test')
+console.log('oprettet reference til test')
+
+//P5 setup() bliver kaldt EN gang før siden vises 
+function setup(){
+    //NU KOMMER DET GENIALE: ONSNAPSHOT 
+    testRef.onSnapshot( snap => {
+        console.log('Modtog snap', snap.size)
+        snap.forEach( doc => {
+            var d = doc.data()
+            console.log(d)
+        })
+    })
+}
+
