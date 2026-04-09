65b5bc8e6334a09e4064bc9f26245fd81839388b
simmoe <simmoe@gmail.com>
2026-04-09 10:01:41 +0200

    firebase quotes init

--- 09_firebase/index.js
+++ 09_firebase/index.js
@@ -1,12 +1,13 @@
 //lav en ref til din collection
-var testRef = db.collection('test')
+var quotesRef = db.collection('quotes_data')
 console.log('oprettet reference til test')
 
 //P5 setup() bliver kaldt EN gang før siden vises 
 function setup(){
     //NU KOMMER DET GENIALE: ONSNAPSHOT 
-    testRef.onSnapshot( snap => {
+    quotesRef.onSnapshot( snap => {
         console.log('Modtog snap', snap.size)
+        //ryd quotes div og sæt de nye quotes ind
         snap.forEach( doc => {
             var d = doc.data()
             console.log(d)
@@ -14,3 +15,26 @@ function setup(){
     })
 }
 
+//key pressed er en indbygget p5.js funktion 
+function keyPressed(){
+    //console.log(key)
+    if(key == "Enter"){
+        //hent teksten fra input feltet
+        var q = select('#newQuote').value()
+        if(q == "") {
+            confirm('skriv venligst noget FØR DU TRYKKER ENTER')
+            return
+        }
+        //nu skal vi gemme det nye quote i firestore
+        //funktionen add() på en collectionref 
+        //OPRETTER en ny collection hvis den IKKE findes 
+        quotesRef.add({
+            text: q,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            //.then kaldes asynkront NÅR add er færdig
+        }).then(
+            console.log('Quote gemt i databasen', q)
+        )
+        select('#newQuote').html('')
+    }
+}
