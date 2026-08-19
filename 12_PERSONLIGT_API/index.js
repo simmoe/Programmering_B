a6aae846903f475ce59249d54b3c60c4dd71d19f
simmoe <simmoe@gmail.com>
2026-08-19 15:15:23 +0200

    api startup

--- 12_PERSONLIGT_API/index.js
+++ 12_PERSONLIGT_API/index.js
@@ -2,5 +2,7 @@
 
 //P5 setup() bliver kaldt EN gang før siden vises 
 function setup(){
+    noCanvas()
     // Brug funktionerne fra dit personlige API her.
+    showToast('SELMA ER FUCKING NICE', 500)
 }
--- 12_PERSONLIGT_API/myApi.js
+++ 12_PERSONLIGT_API/myApi.js
@@ -1,1 +1,15 @@
 // Dine genbrugelige API-funktioner kommer her.
+
+
+//Demands an HTML element with id="toast"
+function showToast(txt, timeout=2000, type="notify"){
+    var toast = select('#toast')
+    toast.html(txt)
+    toast.addClass(type)
+    toast.addClass('toastShow')
+    setTimeout(()=>{
+        toast.removeClass(type)
+        toast.removeClass('toastShow')
+    }, timeout)
+}
+
